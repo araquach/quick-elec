666d98c1f7d74e0a2692947310fe75880400467a
Adam Carter <adam@jakatasalon.co.uk>
2016-10-26 14:59:07 +0000

    A littlemore styling done plus contact page

--- resources/views/page/contact.blade.php
+++ resources/views/page/contact.blade.php
@@ -15,9 +15,69 @@
 @section('content')
 
 <div class="page-content">
+    
     <h2>Contact Us</h2>
-    <p></p>
-    <p></p>
+    
+    <div class="form">
+
+        @if (count($errors) > 0)
+        <div>
+        	<p>Please fix the following input errors:</p>
+        	<ul>
+           		 @foreach($errors->all() as $error)
+                <li>{{{ $error }}}</li>
+            	@endforeach
+        	</ul>
+        </div>
+        @endif
+        
+        {!! Form::open(array('action' => 'SiteController@sendEmail', 'class' => 'form')) !!}
+        
+        	<p>
+        	{!! Form::label('first_name', 'First Name') !!}
+        	{!! Form::text('first_name', old('first_name')) !!}
+        	{!! $errors->first('first_name', '<div class="errorMessage">:message</div>') !!}
+        	</p>
+        	
+        	<p>
+        	{!! Form::label('second_name', 'Second Name') !!}
+        	{!! Form::text('second_name', old('second_name')) !!}
+        	{!! $errors->first('second_name', '<div class="errorMessage">:message</div>') !!}
+        	</p>
+        	
+        	<p>
+        	{!! Form::label('email', 'Email Address') !!}
+        	{!! Form::email('email', old('email')) !!}
+        	{!! $errors->first('email', '<div class="errorMessage">:message</div>') !!}
+        	</p>
+        	
+        	<p>
+        	{!! Form::label('email_confirm', 'Confirm Email Address') !!}
+        	{!! Form::email('email_confirm') !!}
+        	</p>
+        	
+        	<p>
+        	{!! Form::label('mobile', 'Contact Number') !!}
+        	{!! Form::text('mobile', old('mobile')) !!}
+        	{!! $errors->first('mobile', '<div class="errorMessage">:message</div>') !!}
+        	</p>
+        	
+        	<p>
+        	{!! Form::label('body', 'Your Message') !!}
+        	{!! Form::textarea('body', old('body')) !!}
+        	{!! $errors->first('body', '<div class="errorMessage">:message</div>') !!}
+        	</p>
+        	
+        	<p>
+        	{!! Form::submit('Submit') !!}
+        	</p>
+        	
+        {!! Form::close() !!}
+        
+        
+        </div>
+    </div><!-- form -->
+            
 </div>
 
 @stop
