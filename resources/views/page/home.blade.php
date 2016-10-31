3c962678ed0be764b73426e33a62c89dadbd0385
Adam Carter <adam@jakatasalon.co.uk>
2016-10-31 21:20:49 +0000

    More pages styled and slider copy put in place

--- resources/views/page/home.blade.php
+++ resources/views/page/home.blade.php
@@ -11,11 +11,21 @@
 
 <div class="banner">
     <ul>
-        <li class="banner-image banner-image-4"><div class="banner-copy left">Full and partial rewires</div></li>
-        <li class="banner-image banner-image-2"><div class="banner-copy right">Burglar Alarm & cctv installation</div></li>
-        <li class="banner-image banner-image-3"><div class="banner-copy left">Consumer unit upgrades</div></li>
-        <li class="banner-image banner-image-1"><div class="banner-copy right">Lighting updates i.e down lights, led lighting and security lighting</div></li>
-        <li class="banner-image banner-image-5"><div class="banner-copy left">Energy saving upgrades</div></li>
+        <li class="banner-image banner-image-4"><a href="#"><div class="banner-copy left"><span class="header">Full and partial rewires<br></span>
+        <br>Lorem ipsum dolor sit amet, utamur detraxit an eam. Facer exerci similique an sit, erat aeque fastidii eam no, id duo ferri appellantur delicatissimi. Ius eros minimum an.
+        <br><br><strong>Find out more ></strong></div></a></li>
+        <li class="banner-image banner-image-2"><a href="#"><div class="banner-copy right"><span class="header">Burglar Alarm & cctv installation<br></span>
+        <br>Lorem ipsum dolor sit amet, utamur detraxit an eam. Facer exerci similique an sit, erat aeque fastidii eam no, id duo ferri appellantur delicatissimi. Ius eros minimum an.
+        <br><br><strong>More info here ></strong></div></a></li>
+        <li class="banner-image banner-image-3"><a href="#"><div class="banner-copy left"><span class="header">Consumer unit upgrades<br></span>
+        <br>Lorem ipsum dolor sit amet, utamur detraxit an eam. Facer exerci similique an sit, erat aeque fastidii eam no, id duo ferri appellantur delicatissimi. Ius eros minimum an.
+        <br><br><strong>Find out more ></strong></div></a></li>
+        <li class="banner-image banner-image-1"><a href="#"><div class="banner-copy right"><span class="header">Lighting updates<br></span>
+        <br> i.e down lights, led lighting and security lighting Lorem ipsum dolor sit amet, utamur detraxit an eam. Facer exerci similique an sit, erat aeque fastidii eam no, id duo ferri appellantur delicatissimi. Ius eros minimum an.
+        <br><br><strong>Click for more info ></strong></div></a></li>
+        <li class="banner-image banner-image-5"><a href="#"><div class="banner-copy left"><span class="header">Energy saving upgrades<br></span>
+        <br>Lorem ipsum dolor sit amet, utamur detraxit an eam. Facer exerci similique an sit, erat aeque fastidii eam no, id duo ferri appellantur delicatissimi. Ius eros minimum an.
+        <br><br><strong>More here ></strong></div></a></li>
     </ul>
 </div>
 
