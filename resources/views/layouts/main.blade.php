9411a6557022ea8c686fe581b0f6a380332779ab
Adam Carter <adam@jakatasalon.co.uk>
2016-10-31 16:29:37 +0000

    More woork done

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -12,12 +12,14 @@
                 <div id="header-content">
                     <h1>Quick Elec</h1>
                 </div>
-            
+                <a href="{{ URL::to('contact') }}">
+                    <button id="contact-us" class="button--pill">Contact Us</button>
+                </a>
                 <div id="nav">
                     <ul class="list--inline">
                         <li>{!! link_to('/', 'Home') !!}</li>
                         <li>{!! link_to('about', 'About Us') !!}</li>
-                    	<li>{!! link_to('contact', 'Contact Us') !!}</li>
+                    	<li>{!! link_to('contact', 'Contact') !!}</li>
                     	<li>{!! link_to('testimonials', 'Testimonials') !!}</li>
                     </ul>
                 </div>
